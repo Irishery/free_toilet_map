242613a190806be86b969f6d741f52d4d278ecd3
Irishery <markirill21@gmail.com>
2025-08-07 20:51:31 +0200

    minor changes

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "./", // <-- важно для правильных путей к статике
   server: {
+    https: true,
+    host: true,
     proxy: {
       "/toilets": {
         target: process.env.VITE_API_URL || "http://localhost:8080",
